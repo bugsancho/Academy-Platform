db106d4771e642ae5eb3276e51b208dd82125602
bugsancho <bugsancho@gmail.com>
2015-08-16 17:28:23 +0300

    Fixed Category class

---
+++ AcademyPlatform.Models/Courses/Category.cs
@@ -0,0 +1,12 @@
+﻿namespace AcademyPlatform.Models.Courses
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+    }
+}
