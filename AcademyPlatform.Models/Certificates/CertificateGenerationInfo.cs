39916a23d9025168cd9369874186ef3e1231103f
bugsancho <bugsancho@gmail.com>
2016-02-09 05:06:42 +0200

    Adding additional certificate-related files that were ignored

---
+++ AcademyPlatform.Models/Certificates/CertificateGenerationInfo.cs
@@ -0,0 +1,18 @@
+﻿namespace AcademyPlatform.Models.Certificates
+{
+    public class CertificateGenerationInfo
+    {
+        public string TemplateFilePath { get; set; }
+
+        public string BaseFilePath { get; set; }
+
+        public PlaceholderInfo StudentName { get; set; }
+
+        public PlaceholderInfo CourseName { get; set; }
+
+        public PlaceholderInfo IssueDate { get; set; }
+
+        public PlaceholderInfo QrCode { get; set; }
+
+    }
+}
